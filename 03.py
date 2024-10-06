b0527998d9f302b351c329f21358c50f9d8ce2ba
kangactor123 <kangactor123@naver.com>
2024-10-06 14:32:05 +0900

    래벨 0 문제 추가

--- 03.py
+++ 03.py
@@ -1,7 +1,6 @@
 # 파이썬 연습
-# 접미사인지 확인하기
-## 프로그래머스 0 레벨
 
+# 접미사인지 확인하기
 def initList(my_string):
     suffix_list = []
     for idx in range(len(my_string)):
@@ -16,4 +15,21 @@ def solution(my_string, is_suffix):
     if is_suffix in suffix_list:
         answer = 1
     
+    return answer
+
+# 글자 이어 붙여 문자열 만들기
+def solution(my_string, index_list):
+    answer = ''
+    for idx in index_list:
+        answer += my_string[idx]
+    return answer
+
+# 수 조작하기 1
+def solution(n, control):
+    answer = n
+    map = { "w": 1, "s": -1, "d": 10, "a": -10 }
+    
+    for char in control:
+        answer += map[char]
+
     return answer
